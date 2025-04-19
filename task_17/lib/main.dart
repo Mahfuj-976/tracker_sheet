75ecde2c6ea4304bfc75f7a96b5e5cb28cd65d8a
Mahfuj-976 <mahfujurrahman976@gmail.com>
2025-04-19 22:41:28 +0600

    task 17

---
+++ task_17/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void _toggleDarkMode(bool value) {
+    setState(() {
+      _isDarkMode = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Change the Mode',
+      theme:
+          _isDarkMode
+              ? ThemeData.dark()
+              : ThemeData.light(),
+      home: SettingsScreen(
+        isDarkMode: _isDarkMode,
+        onToggleDarkMode: _toggleDarkMode,
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  final bool isDarkMode;
+  final Function(bool) onToggleDarkMode;
+
+  SettingsScreen({
+    required this.isDarkMode,
+    required this.onToggleDarkMode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task 17'),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Setting',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Dark Mode',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Switch(
+                  value: isDarkMode,
+                  onChanged: onToggleDarkMode,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
